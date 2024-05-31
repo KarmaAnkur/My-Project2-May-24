c8dc5bf04d2ef3a4a213748e0393c7fc3129b47b
Singh <Ankur.Singh@mind.motherson.com>
2024-05-31 11:01:02 +0530

    1st

---
+++ script.js
@@ -0,0 +1,19 @@
+// script.js
+function fetchLambdaInfo() {
+    fetch('https://your-api-gateway-url/lambdas')
+        .then(response => response.json())
+        .then(data => displayData(data))
+        .catch(error => console.error('Error:', error));
+}
+
+function fetchS3Info() {
+    fetch('https://your-api-gateway-url/s3buckets')
+        .then(response => response.json())
+        .then(data => displayData(data))
+        .catch(error => console.error('Error:', error));
+}
+
+function displayData(data) {
+    const contentDiv = document.getElementById('content');
+    contentDiv.innerHTML = JSON.stringify(data, null, 2);
+}
